7cbdc37bd81f6ea3159b54434096eb90ef8c589a
Andrej Warkentin <andrej@laffitte.bccn-berlin.pri>
2015-07-31 12:19:53 +0200

    almost final version of plotting code

--- Running_and_Plotting.py
+++ Running_and_Plotting.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 import our_functions as RL
@@ -5,6 +7,7 @@ plt.style.use('ggplot')
 
 import matplotlib as mpl
 mpl.rcParams['font.size'] = 20
+mpl.rcParams['lines.linewidth']= 3
 
 def plot_learning_curve(steps_needed, N_rats, N_episodes, plottitle):
     plt.figure(figsize=(12,8))
@@ -15,7 +18,7 @@ def plot_learning_curve(steps_needed, N_rats, N_episodes, plottitle):
     plt.ylabel("Number of steps needed")
     
 def plot_multiple_learning_curve(steps_needed_list, labels, N_rats, N_episodes, plottitle):
-    plt.figure(figsize=(12,8))
+    plt.figure(figsize=(16,12))
     for i in range(len(steps_needed_list)):
         plt.plot(np.arange(1,N_episodes+1), steps_needed_list[i],label=labels[i])
     plt.legend()
@@ -30,59 +33,106 @@ def exponential_epsilon_decline(episode_number):
 def reverse_exponential_epsilon(episode_number):
     return 1 - 1.5**(-episode_number) + .1
     
+def linear_epsilon(episode_number):
+    return 0.85 - 0.1*episode_number + .1
+    
+def static_epsilon(value):
+    def func(episode):
+        return value
+    return func
+    
 
 ################################################################################
 ## Point 1: Averaging over 10 rats and plot the learning curve
-#N_rats = 10
-#N_episodes = 25
-#plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
-#            " using standard parameters"
-#steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline)
-#plot_learning_curve(steps_needed, N_rats, N_episodes, plottitle)
-#plt.savefig('plots/learning_curve_standard.png',format='png')
+N_rats = 10
+N_episodes = 25
+plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
+            " using standard parameters"
+steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline)
+plot_learning_curve(steps_needed, N_rats, N_episodes, plottitle)
+plt.savefig('plots/learning_curve_standard.png',format='png')
 
 ###############################################################################
-# Point 2: Plotting the navigation map of the animal after different numbers of
+# Point 2: Plotting the navigation map of the animal after different numbers of 
 # trials
 N_rats = 1
-N_episodes = 10
+N_episodes = 25
 steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline,
-                             [2, 6, 10], weight_decay=False)
+                             [2, 6, 25], weight_decay=False)
                              
 ################################################################################
 ## Point 3: Comparing for different values of eligibility trace
-#N_rats = 5
-#N_episodes = 30
-#lambda_ = 0
-#
-#steps_needed_list = []
-#labels = []
-#plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
-#            " with lambda = " + str(lambda_)
-#label = '$\lambda$ = ' +str(lambda_)
-#labels.append(label)
-#steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline, \
-#                             lambda_=lambda_)
-#steps_needed_list.append(steps_needed)
-#
-#
-#lambda_ = .95
-#plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
-#            " with lambda = " + str(lambda_)
-#steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline, \
-#                             lambda_=lambda_)
-#label = '$\lambda$ = ' +str(lambda_)
-#labels.append(label)
-#steps_needed_list.append(steps_needed)
-#plot_multiple_learning_curve(steps_needed_list,labels, N_rats, N_episodes, plottitle)
-#plt.savefig('plots/learning_curves_lambda.png',format='png')
+N_rats = 5
+N_episodes = 30
+lambda_ = 0
+
+steps_needed_list = []
+labels = []
+plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
+            " with lambda = " + str(lambda_)
+label = '$\lambda$ = ' +str(lambda_)
+labels.append(label)
+steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline, \
+                             lambda_=lambda_)
+steps_needed_list.append(steps_needed)
+
+
+lambda_ = .95
+plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
+            " with lambda = " + str(lambda_)
+steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline, \
+                             lambda_=lambda_)
+label = '$\lambda$ = ' +str(lambda_)
+labels.append(label)
+steps_needed_list.append(steps_needed)
+plot_multiple_learning_curve(steps_needed_list,labels, N_rats, N_episodes, plottitle)
+plt.savefig('plots/learning_curves_lambda.png',format='png')
 ################################################################################
 ## Point 4: Varying the time course of the exploration / exploitation parameter
-#steps_needed = RL.run_trials(N_rats, N_episodes, reverse_exponential_epsilon)
-#plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
-#            " with increasing epsilon"
-#plot_learning_curve(steps_needed, N_rats, N_episodes, plottitle)
-#plt.savefig('plots/learning_curve_epsilonUp.png',format='png')
+N_rats = 10
+N_episodes = 10
+steps_needed_list = []
+labels = []
+
+plottitle = "Needed steps averaged over " + str(N_rats) + " rats" \
+            " with increasing epsilon"
+            
+# Compare dynamic time courses for epsilon
+steps_needed = RL.run_trials(N_rats, N_episodes, exponential_epsilon_decline)
+steps_needed_list.append(steps_needed)
+label = 'exp. decrease of $\epsilon$'
+labels.append(label)
+steps_needed = RL.run_trials(N_rats, N_episodes, reverse_exponential_epsilon)
+steps_needed_list.append(steps_needed)
+label = 'exp. increase of $\epsilon$'
+labels.append(label)
+steps_needed = RL.run_trials(N_rats, N_episodes, linear_epsilon)
+steps_needed_list.append(steps_needed)
+label = 'linear decrease of $\epsilon$'
+labels.append(label)
+
+plot_multiple_learning_curve(steps_needed_list, labels, N_rats, N_episodes, plottitle)
+plt.savefig('plots/learning_curve_epsilonDynamic.png',format='png')
+
+# compare static values for epsilon
+steps_needed_list = []
+labels = []
+steps_needed = RL.run_trials(N_rats, N_episodes, static_epsilon(0.1))
+steps_needed_list.append(steps_needed)
+label = 'static $\epsilon$=0.1'
+labels.append(label)
+steps_needed = RL.run_trials(N_rats, N_episodes, static_epsilon(0.5))
+steps_needed_list.append(steps_needed)
+label = 'static $\epsilon$=0.5'
+labels.append(label)
+steps_needed = RL.run_trials(N_rats, N_episodes, static_epsilon(0.75))
+steps_needed_list.append(steps_needed)
+label = 'static $\epsilon$=0.75'
+labels.append(label)
+
+plot_multiple_learning_curve(steps_needed_list, labels, N_rats, N_episodes, plottitle)
+plt.savefig('plots/learning_curve_epsilonStatic.png',format='png')
+
 ################################################################################
 ## Point 5: Varying the number of directions
 #N_rats = 1
